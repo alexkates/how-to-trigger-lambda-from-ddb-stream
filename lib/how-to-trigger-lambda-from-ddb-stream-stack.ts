e5fce4221e0a2f015dd3fa3150c589c18b3db6b6
Daniel Alexander Kates <alex.kates@creditly.com>
2021-05-14 15:06:38 -0400

    implemented ddb table, lambda function and event source

--- lib/how-to-trigger-lambda-from-ddb-stream-stack.ts
+++ lib/how-to-trigger-lambda-from-ddb-stream-stack.ts
@@ -1,9 +1,28 @@
 import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { DynamoEventSource } from '@aws-cdk/aws-lambda-event-sources';
 
 export class HowToTriggerLambdaFromDdbStreamStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamodb.Table(this, 'Table', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+      tableName: 'Table',
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'TableStreamHandler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
+    lambdaFunction.addEventSource(new DynamoEventSource(table, {
+      startingPosition: lambda.StartingPosition.LATEST,
+    }));
   }
 }
